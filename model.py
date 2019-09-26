aaf8dbbe2365199e72a0d731af03e06386d93bb9
Robin970822 <717740827@qq.com>
2019-09-26 17:03:46 +0800

    save, load and choose model

--- model.py
+++ model.py
@@ -1,30 +1,28 @@
 import tensorflow as tf
 
 
-def get_model(input_shape, output_shape):
-    '''
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.Conv2D(filters=4, kernel_size=3,
-                               activation=tf.nn.relu, padding='same'),
-        tf.keras.layers.MaxPool2D(),
-        tf.keras.layers.Conv2D(filters=8, kernel_size=3,
-                               activation=tf.nn.relu),
-        tf.keras.layers.Conv2D(filters=16, kernel_size=3,
-                               activation=tf.nn.relu),
-        tf.keras.layers.Dense(128, activation=tf.nn.relu),
-        tf.keras.layers.Dense(output_shape, activation=tf.nn.softmax)
-    ])
-    '''
-
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.Flatten(input_shape=input_shape),
-        tf.keras.layers.Dense(128, activation=tf.nn.relu),
-        tf.keras.layers.Dense(256, activation=tf.nn.relu),
-        tf.keras.layers.Dense(128, activation=tf.nn.relu),
-        tf.keras.layers.Dense(output_shape, activation=tf.nn.softmax)
-    ])
+def get_model(input_shape, output_shape, model_type='FC'):
+    if model_type == 'FC':
+        model = tf.keras.models.Sequential([
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Flatten(input_shape=input_shape),
+            tf.keras.layers.Dense(128, activation=tf.nn.relu),
+            tf.keras.layers.Dense(256, activation=tf.nn.relu),
+            tf.keras.layers.Dense(128, activation=tf.nn.relu),
+            tf.keras.layers.Dense(output_shape, activation=tf.nn.softmax)
+        ])
+    elif model_type == 'CNN':
+        model = tf.keras.models.Sequential([
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Conv2D(filters=4, kernel_size=3,
+                                   activation=tf.nn.relu, padding='same'),
+            tf.keras.layers.MaxPool2D(),
+            tf.keras.layers.Conv2D(filters=8, kernel_size=3,
+                                   activation=tf.nn.relu),
+            tf.keras.layers.Conv2D(filters=16, kernel_size=3,
+                                   activation=tf.nn.relu),
+            tf.keras.layers.Dense(128, activation=tf.nn.relu),
+            tf.keras.layers.Dense(output_shape, activation=tf.nn.softmax)])
 
     model.compile(optimizer='adam',
                   loss='sparse_categorical_crossentropy',
--- train.py
+++ train.py
@@ -1,10 +1,20 @@
 import config
 import os
+import time
+import argparse
 
 import numpy as np
-from model import get_model
+from model import get_model, load_model
 from utils import crop_from_img, generate_from_file_list, generate_from_file_list
 
+# args
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', help='input path for model')
+parser.add_argument('-o', '--output', help='output path for model', default='{}.h5'.format(time.time()))
+parser.add_argument('-m', '--model_type', help='model type', default='FC')
+args = parser.parse_args()
+
+# Training Data
 pos_list = config.pos_list
 neg_list = config.neg_list
 
@@ -15,9 +25,7 @@ print('Pos: {} Neg: {}'.format(len(pos_train), len(neg_train)))
 x_train = np.concatenate((pos_train, neg_train), axis=0)
 y_train = np.hstack((np.ones(len(pos_train)), np.zeros(len(neg_train))))
 
-# Train
-model = get_model(input_shape=(10, 10), output_shape=2)
-
+# Test Data
 pos_test = np.load('F:\\GitHub\\DICOM\\coordinate-07\\0710_60day_20190911_143703SWI\\pos.npy')
 neg_test = np.load('F:\\GitHub\\DICOM\\testData2\\20190309\\neg.npy')
 pos_test = pos_test[:, :, :, np.newaxis]
@@ -25,6 +33,12 @@ neg_test = neg_test[:, :, :, np.newaxis]
 
 pos_y = np.ones(len(pos_test))
 neg_y = np.zeros(len(neg_test))
+
+# Train
+if args.input:
+    model = load_model(args.input)
+else:
+    model = get_model(input_shape=(10, 10), output_shape=2, model_type=args.model_type)
 
 model.fit(x_train, y_train, epochs=1000, batch_size=256,
           validation_data=(pos_test, pos_y), validation_freq=100, verbose=1)
@@ -34,5 +48,5 @@ model.evaluate(pos_test, pos_y)
 model.evaluate(neg_test, neg_y)
 model.evaluate(np.concatenate((pos_test, neg_test), axis=0), np.concatenate((pos_y, neg_y), axis=0))
 
-model_path = os.path.join(config.model_path, 'CNN_h3_e1000.h5')
+model_path = os.path.join(config.model_path, args.output)
 model.save(model_path)
